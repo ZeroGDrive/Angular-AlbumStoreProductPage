7b2044fd9e8300a1f4f5215dca9d5827af6da14f
Ayoub Alfurjani <58040995+ZeroGDrive@users.noreply.github.com>
2021-11-18 10:36:56 +0200

    part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
-import { ProductPageComponent } from './product-page/product-page.component';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { ProductDescriptionComponent } from "./product-description/product-description.component";
+import { ProductPageComponent } from "./product-page/product-page.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductPageComponent
+    ProductPageComponent,
+    ProductDescriptionComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/product-description/product-description.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-product-description",
+  templateUrl: "product-description.component.html",
+})
+export class ProductDescriptionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
